129cba5bdcf33083d68c3fe833aa94ce764ba087
Devin Miller <devin.miller@usfoods.com>
2023-07-21 17:03:57 -0700

    Mappings

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -2,7 +2,7 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("FileType", {
     pattern = "terraform, json",
-    callback = function ()
+    callback = function()
         vim.opt.shiftwidth = 2
         vim.opt.softtabstop = 2
         vim.opt.tabstop = 2
@@ -11,7 +11,5 @@ autocmd("FileType", {
 
 autocmd("BufWritePre", {
     pattern = "*",
-    callback = function ()
-        vim.lsp.buf.format()
-    end
+    callback = function() vim.lsp.buf.format() end
 })
--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,8 +1,27 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move highlighted text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Cursor remains in place when appending
 vim.keymap.set("n", "J", "mzJ`z")
+-- Keep cursor centered paging up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keep cursor centered when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Better indent handling in visual mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Better escape using jk in insert and terminal mode
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("t", "jk", "<C-\\><C-n>")
+
+-- Mapping commands for nvim tree
+vim.keymap.set("n", "<C-m>", "<cmd> NvimTreeFocus <CR>")
+vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>")
